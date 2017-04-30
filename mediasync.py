efff2cb0632d37b3adb4cad11eda477367cf3de5
Fabian Paul <fabian.paul@student.hpi.de>
2017-04-30 17:00:42 +0200

    Generate multimedia list from subdirs

--- mediasync.py
+++ mediasync.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+from collections import deque
 
 
 def main():
@@ -6,8 +8,8 @@ def main():
                                                   multimedia file lists')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-g', '--generate',
-                       help='List all leaf directories under a specific root \
-                       directory.')
+                       help='Lists all leaf directories under a specific root \
+                       directory and saves a file.')
     group.add_argument('-c', '--compare', nargs=2,
                        help='Takes two multimedia list paths and compares \
                              them.')
@@ -27,7 +29,22 @@ def compare(first, second):
 
 
 def generate(root):
-    pass
+    queue = deque(_build_paths(root))
+    leafs = []
+    while queue:
+        current = queue.popleft()
+        subdirs = _build_paths(current)
+        if not subdirs:
+            leafs.append(current.split('/')[-1])
+            continue
+        queue.extend(subdirs)
+    with open('multimedia.txt', 'w') as f:
+        f.write('\n'.join(leafs))
+
+
+def _build_paths(prefix):
+    return [os.path.join(prefix, x) for x in next(os.walk(prefix))[1]]
+
 
 if __name__ == '__main__':
     main()
